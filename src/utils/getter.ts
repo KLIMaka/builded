f3cf1badf4fbbc14982e03491c5f65bc41366096
KLIMaka <klimaka01@gmail.com>
2020-06-26 13:07:12 +0300

    rewrite getter to fetch

--- src/utils/getter.ts
+++ src/utils/getter.ts
@@ -1,37 +1,9 @@
-const cache: { [index: string]: any } = {};
-const xhrCache: { [index: string]: XMLHttpRequest } = {}
-const cacheString: { [index: string]: any } = {};
-const xhrCacheString: { [index: string]: XMLHttpRequest } = {}
-
-function getRequest(fname: string, type: XMLHttpRequestResponseType, cache: { [index: string]: XMLHttpRequest }) {
-  let xhr = cache[fname];
-  if (xhr == undefined) {
-    xhr = new XMLHttpRequest();
-    xhr.responseType = type;
-    xhr.addEventListener('load', () => delete cache[fname]);
-    xhr.open('GET', fname, true);
-    xhr.send();
-    cache[fname] = xhr;
-  }
-  return xhr;
+export async function loadBin(fname: string): Promise<ArrayBuffer> {
+  const r = await fetch(fname);
+  return r.ok ? r.arrayBuffer() : null;
 }
 
-export function loadBin(fname: string, progressCallback: (percent: number) => void = () => { }): Promise<ArrayBuffer> {
-  const file = cache[fname];
-  if (file != undefined) return Promise.resolve(file);
-  return new Promise((resolve) => {
-    const xhr = getRequest(fname, 'arraybuffer', xhrCache);
-    xhr.addEventListener('load', () => { if (xhr.status != 200) { resolve(null); return } cache[fname] = xhr.response; resolve(xhr.response) });
-    xhr.addEventListener('error', () => { resolve(null) });
-    xhr.addEventListener('progress', (e) => { progressCallback(e.loaded / e.total); });
-  });
-}
-
-export function loadString(fname: string): Promise<string> {
-  const str = cacheString[fname];
-  if (str != undefined) return Promise.resolve(str);
-  return new Promise((resolve) => {
-    const xhr = getRequest(fname, 'text', xhrCacheString);
-    xhr.addEventListener('load', () => { cacheString[fname] = xhr.response; resolve(xhr.response); });
-  })
+export async function loadString(fname: string): Promise<string> {
+  const r = await fetch(fname);
+  return r.ok ? r.text() : null;
 }
